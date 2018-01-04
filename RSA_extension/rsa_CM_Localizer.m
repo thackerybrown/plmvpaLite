4c7de88d3c2bc03d362c964785c2e726bc2e87df
thackerybrown <thackeryibrown@gmail.com>
2018-01-03 19:24:05 -0500

    update RSA extension to assign TR-level condition names

--- RSA_extension/rsa_CM_Localizer.m
+++ RSA_extension/rsa_CM_Localizer.m
@@ -4,12 +4,16 @@ function [] = rsa_CM_Localizer(Sub, Mask, TRsperRun)
 %For alternate code demo purposes, this is built around both 4D and 3D
 %image file types. The use case for 4D includes scenarios like "raw" BOLD data or residual time-series
 
+%warning: as of 1/3/2018; unconcatenated 4D analysis has not been debugged.
+%at this time only use concatenated 3D
+
 %% flags and parameters
 S.TR = 2;
 theseTRWeights = [0 0.25 0.5 0.25 0];
 funcftype = '.nii';
-runs_concat = 0; %1 = typical SPM analysis; will have continuous onsets concatenated across runs. 0 = But you might not have bothered creating such a file, or in SST case we are using files from FSL. In this case, the onsets are assumed to "reset" for each run ('raw' unconcatenated onsets)
+runs_concat = 1; %1 = typical SPM analysis; will have continuous onsets concatenated across runs. 0 = But you might not have bothered creating such a file, or in SST case we are using files from FSL. In this case, the onsets are assumed to "reset" for each run ('raw' unconcatenated onsets)
 
+gen_onsetsTR = 1; %1=yes. Typically, you'll use an onsets.mat file with tr-by-tr onsets and names (as used for a beta-series). But if you only have a traditional GLM model with one name for multile onsets, setting this flag to 1 will auto-populate unique but related names (e.g., Face_1; Face_2...)
 %sub = Sub{1};
 
 %Subject ID/number
@@ -29,7 +33,6 @@ preproc_lvl = ''; % 'a' for slice-time-only, 'u' for realigned-only, 'ua' for re
 boldnames = [preproc_lvl 'run']; %name of image files with preprocessing level prefix
 
 ImgDims = 3; %if working with timeseries, it is highly recommended that you use 4D nifti files ('4'). If you have split them out into TR-by-TR, or are working with betas, enter '3'
-
 
 %% Directories
 S.expt_dir = ['/home/brain/host/mvpa_sample_data/' S.exp_name '/'];%study location
@@ -53,24 +56,144 @@ end
 S.anat_dir = [S.expt_dir S.subj_id '/Masks'];
 maskname=[S.anat_dir '/' mask '.nii'];
 
-
 S.group_mvpa_dir = [S.expt_dir 'RSA_output_files'];%results .mat files are spit out in here
-
-
-%basepath = ['/home/brain/host/mvpa_sample_data/' exp_name '/'];%'/Volumes/group/awagner';
-
-%
-
-%subj '_localizer_onsets_test'];
-
-%mvpa_dir = [basepath study_prefix sub '/results01/'];
-
 
 %% extract patterns
 if runs_concat == 1
-    a = [];
+    %load onsets
+    load([S.mvpa_dir S.onsets_filename]);
     
-else
+    %runs = [1 2]; %indicate number of runs and which ones to target with analysis
+    
+    rmat_condensed = [];
+    onsets_TRs = [];
+    names_TRs = [];%filled in if gen_onsetsTR == 1
+    
+    %before loading and preprocessing all the pattern data, we can see if its
+    %already there for analysis
+    if exist([S.mvpa_dir '/RSA_data/' S.subj_id '_' mask '_condensedpats.mat']);
+        load([S.mvpa_dir '/RSA_data/' S.subj_id '_' mask '_condensedpats.mat']);
+        load([S.mvpa_dir '/RSA_data/' S.subj_id '_onsets_expanded.mat']);
+    else
+        
+        %% load in pattern data
+        if ImgDims == 4
+            
+            a = [];
+            
+        elseif ImgDims == 3
+            
+            
+            runfolds = dir(fullfile(par.funcdir, 'run_*'));%dir(fullfile(par.funcdir, 'localizer*'));%
+            for idxr = 1:length(runfolds)
+                allrawfilenames{idxr,1} = dir(fullfile(par.funcdir, runfolds(idxr).name, ['/' boldnames '*.nii']));%'/swa*.nii'));%
+                
+                %if 3D images (not recommended) check if the count matches that
+                %specified for other stages of the process
+                if ImgDims == 3
+                    if length(allrawfilenames{idxr})~=TRsperRun(idxr);
+                        error('your specified run length does not match 3D file count')
+                    end
+                end
+                
+                for idxf = 1:length(allrawfilenames{idxr})
+                    allrawfilepaths{idxr,1}{idxf,1} = runfolds(idxr).name;
+                end
+            end
+            allrawfilenames = vertcat(allrawfilenames{:});
+            allrawfilepaths = vertcat(allrawfilepaths{:});
+            for idx = 1:length(allrawfilenames);
+                raw_filenames{idx,1} = [par.funcdir char(allrawfilepaths(idx)) '/' allrawfilenames(idx).name];
+            end
+            
+            %files may have been read in out of order. This would be very very bad.
+            %Here, we try to confirm/fix this with a resort - but you *MUST* double
+            %check that the final file order is correct before proceeding with
+            %analysis
+            for idx = 1:length(raw_filenames)
+                %first, identify the image number from its name in full
+                %('001' from run_001.nii)
+                nifti_indices = strfind(raw_filenames{idx,1}, '.nii'); %assuming .nii, where does that fall in the string?
+                underscore_indices = strfind(raw_filenames{idx,1}, '_'); %assuming the number is preceded by '_', where are the underscores?
+                imnum = str2double(raw_filenames{idx,1}(underscore_indices(end)+1:nifti_indices(end)-1));
+                raw_filenames{idx,2} = imnum;
+                %if length(raw_filenames{idx,1}) == 100%80
+                %    raw_filenames{idx,2} = str2double(raw_filenames{idx,1}(length(raw_filenames{idx,1})-9:length(raw_filenames{idx,1})-9));
+                %else
+                %    raw_filenames{idx,2} = str2double(raw_filenames{idx,1}(length(raw_filenames{idx,1})-10:length(raw_filenames{idx,1})-9));
+                %end
+                
+            end
+            
+            a = sortrows(raw_filenames, 2);
+            raw_filenames = a(:,1);
+            
+            %if the BOLD images are 3D instead of 4D, we need to modify indices further to avoid introducing a new sorting error
+            
+            for idx = 1:length(raw_filenames)
+                %first, identify the RUN number from its name in full
+                runref_indices = strfind(raw_filenames{idx,1}, '/run_');
+                runidxnum = str2double(raw_filenames{idx,1}(runref_indices(1)+5:runref_indices(2)-1));
+                raw_filenames{idx,3} = runidxnum;
+            end
+            
+            b = sortrows(raw_filenames, 3);
+            raw_filenames = b(:,1);
+            
+            imgslength = length(raw_filenames);
+            
+            %% iterate through 3D frames to extract all patterns
+            for i=1:imgslength
+                betamaps{i} = raw_filenames{i};%[tmp.name ',' num2str(i)];
+                
+                [b,r] = MAP_getROI(maskname, betamaps{i}, 'vox', 0, '');
+                bmat_t(:,i) = b{1}; % returns all voxels, whether or not they have NaNs
+                rmat_t(:,i) = r; % returns voxels excluding NaNs
+                %meanbetas_t = nanmean(bmat_t(:,:));%get mean beta values from the ROI for each regressor
+                
+            end
+            
+            %% now compute a mean pattern for __ TRs surrounding the onset
+            
+            for n = 1:length(names)
+                
+                
+                time_idx = floor(onsets{n}/S.TR) + 1;%convert onsets to TRs
+                
+                onsets_TR{n} = time_idx; %sort(horzcat(onsets_t{n}, onsets_t{n}{idxThisCond}(enoughTRs_h)));%put the onsets for cond{i} into an array,
+                
+                theseTRWeights2 = theseTRWeights;
+                
+                %create weighted mean pattern for that onset
+                for nvoxr = 1:length(rmat_t)
+                    %tempvals = [];
+                    for tidx = 1:length(time_idx)
+                        tempvals = theseTRWeights2.*rmat_t(nvoxr,time_idx(tidx):time_idx(tidx)+(length(theseTRWeights2)-1));
+                        condmat_condensed_t(nvoxr,tidx) = squeeze(sum(tempvals));
+                    end
+                end
+                rmat_condensed = [rmat_condensed condmat_condensed_t];
+                
+                if gen_onsetsTR == 1
+                    for tidx = 1:length(time_idx)
+                        tempnames{tidx} = [names{n} '_' num2str(tidx)];
+                    end
+                end
+                
+                names_TRs = [names_TRs tempnames];
+            end
+        end
+        
+        
+        %% write out pattern info
+        savename1 = [S.mvpa_dir '/RSA_data/' S.subj_id '_' mask '_condensedpats.mat'];
+        save(savename1,'rmat_condensed');
+        
+        savename2 = [S.mvpa_dir '/RSA_data/' S.subj_id '_onsets_expanded.mat'];
+        save(savename2,'names','onsets','names_TRs','durations');
+    end
+    
+else %in debugging stage as of 1/3/2018
     %load onsets
     load([S.mvpa_dir S.onsets_filename]);
     
@@ -166,145 +289,17 @@ else
             
         elseif ImgDims == 3
             
+            a=[];
             
-            runfolds = dir(fullfile(par.funcdir, 'run_*'));%dir(fullfile(par.funcdir, 'localizer*'));%
-            for idxr = 1:length(runfolds)
-                allrawfilenames{idxr,1} = dir(fullfile(par.funcdir, runfolds(idxr).name, ['/' boldnames '*.nii']));%'/swa*.nii'));%
-                
-                %if 3D images (not recommended) check if the count matches that
-                %specified for other stages of the process
-                if ImgDims == 3
-                    if length(allrawfilenames{idxr})~=TRsperRun(idxr);
-                        error('your specified run length does not match 3D file count')
-                    end
-                end
-                
-                for idxf = 1:length(allrawfilenames{idxr})
-                    allrawfilepaths{idxr,1}{idxf,1} = runfolds(idxr).name;
-                end
-            end
-            allrawfilenames = vertcat(allrawfilenames{:});
-            allrawfilepaths = vertcat(allrawfilepaths{:});
-            for idx = 1:length(allrawfilenames);
-                raw_filenames{idx,1} = [par.funcdir char(allrawfilepaths(idx)) '/' allrawfilenames(idx).name];
-            end
-            
-            %files may have been read in out of order. This would be very very bad.
-            %Here, we try to confirm/fix this with a resort - but you *MUST* double
-            %check that the final file order is correct before proceeding with
-            %analysis
-            for idx = 1:length(raw_filenames)
-                %first, identify the image number from its name in full
-                %('001' from run_001.nii)
-                nifti_indices = strfind(raw_filenames{idx,1}, '.nii'); %assuming .nii, where does that fall in the string?
-                underscore_indices = strfind(raw_filenames{idx,1}, '_'); %assuming the number is preceded by '_', where are the underscores?
-                imnum = str2double(raw_filenames{idx,1}(underscore_indices(end)+1:nifti_indices(end)-1));
-                raw_filenames{idx,2} = imnum;
-                %if length(raw_filenames{idx,1}) == 100%80
-                %    raw_filenames{idx,2} = str2double(raw_filenames{idx,1}(length(raw_filenames{idx,1})-9:length(raw_filenames{idx,1})-9));
-                %else
-                %    raw_filenames{idx,2} = str2double(raw_filenames{idx,1}(length(raw_filenames{idx,1})-10:length(raw_filenames{idx,1})-9));
-                %end
-                
-            end
-            
-            a = sortrows(raw_filenames, 2);
-            raw_filenames = a(:,1);
-            
-            %if the BOLD images are 3D instead of 4D, we need to modify indices further to avoid introducing a new sorting error
-            
-            for idx = 1:length(raw_filenames)
-                %first, identify the RUN number from its name in full
-                runref_indices = strfind(raw_filenames{idx,1}, '/run_');
-                runidxnum = str2double(raw_filenames{idx,1}(runref_indices(1)+5:runref_indices(2)-1));
-                raw_filenames{idx,3} = runidxnum;
-            end
-            
-            b = sortrows(raw_filenames, 3);
-            raw_filenames = b(:,1);
-            
-            
-            
-            
-            %for rnum = 1:length(TRsperRun)
-            
-            %     run = num2str(rnum);
-            
-            %     path = [basepath study_prefix sub '/bolds/run_' run '/'];
-            
-            %     cd(path);
-            
-            
-            %     tmp=dir(['*' funcftype]);
-            
-            %     runlength = length(spm_vol(strtok(tmp.name)));
-            imgslength = length(raw_filenames);
-            
-            %iterate through 3D frames to extract all patterns
-            for i=1:imgslength
-                betamaps{i} = raw_filenames{i};%[tmp.name ',' num2str(i)];
-                
-                [b,r] = MAP_getROI(maskname, betamaps{i}, 'vox', 0, '');
-                bmat_t(:,i) = b{1}; % returns all voxels, whether or not they have NaNs
-                rmat_t(:,i) = r; % returns voxels excluding NaNs
-                %meanbetas_t = nanmean(bmat_t(:,:));%get mean beta values from the ROI for each regressor
-                
-            end
-            
-            %%      now compute a mean pattern for __ TRs surrounding the onset; on a run-by-run basis
-           % for rnum = 1:length(TRsperRun)
-                
-
-                
-                for n = 1:length(names)
-                %names_t = names{n}(cell2mat(runs_onsidx)==rnum);%filter to current run
-                %onsets_t = onsets{n}(cell2mat(runs_onsidx)==rnum);%filter to current run
-                    
-                    time_idx = floor(onsets{n}/S.TR) + 1;%convert onsets to TRs
-                    
-                %    enoughTRs_h = (time_idx + (length(theseTRWeights)-1)) <= TRsperRun(rnum);%runlength;
-                %    lengthdiff = runlength - time_idx; %calculate how many more TRs there are beyond the onset
-                    %onsets{i} = sort(horzcat(onsets{i}, theseOnsets(enoughTRs_h)));
-                    onsets_TR{n} = time_idx; %sort(horzcat(onsets_t{n}, onsets_t{n}{idxThisCond}(enoughTRs_h)));%put the onsets for cond{i} into an array,
-                    
-                    %if the n TRs is too short, calculate a new weights vector
-                    %to accomodate
-                %    if enoughTRs_h == 1
-                        theseTRWeights2 = theseTRWeights;
-                %    else
-                %        theseTRWeights2 = theseTRWeights(1:(lengthdiff+1))/(sum(theseTRWeights(1:(lengthdiff+1))));
-                %    end
-                    %restricting to onsets for which we have enough TRs acquired.
-                    %NOTE: This will literally REcreate the onsets component of my model files,
-                    %BUT is still useful IF we haven't prescreened them based
-                    %on how many TRs we acquired (i.e. we might get 31/32
-                    %onsets of the 32nd exceeds the number of TRs we acquired,
-                    %based on how many post-onset TRs we want to average across
-                    %in our MVPA analysis
-                    
-                    %create weighted mean pattern for that onset
-                    for nvoxr = 1:length(rmat_t)
-                        %tempvals = [];
-                        for tidx = 1:length(time_idx)
-                        tempvals = theseTRWeights2.*rmat_t(nvoxr,time_idx(tidx):time_idx(tidx)+(length(theseTRWeights2)-1));
-                        condmat_condensed_t(nvoxr,tidx) = squeeze(sum(tempvals));
-                        end
-                        %rmat_condensed_t(nvoxr,n) = squeeze(sum(tempvals));
-                    end
-                    rmat_condensed = [rmat_condensed condmat_condensed_t];
-                end
-                %% now integrate patterns and indices into master matrices for study-level analysis
-                %rmat_condensed = [rmat_condensed _condensed_t];
-                onsets_TRs = [onsets_TRs onsets_TR];
-            %end
         end
+        
+        %% write out pattern info
+        savename1 = [basepath '/wagner/thackery/SST_temp/sst' sub '/mvpa_orient/sst' sub '_' mask '_condensedpats.mat'];
+        save(savename1,'rmat_condensed');
+        
+        savename2 = [basepath '/wagner/thackery/SST_temp/sst' sub '/mvpa_orient/onsets_nonconcat.mat'];
+        save(savename2,'names','onsets','onsets_TRs','durations','runs_onsidx');
     end
-    %% write out pattern info
-    savename1 = [basepath '/wagner/thackery/SST_temp/sst' sub '/mvpa_orient/sst' sub '_' mask '_condensedpats.mat'];
-    save(savename1,'rmat_condensed');
-    
-    savename2 = [basepath '/wagner/thackery/SST_temp/sst' sub '/mvpa_orient/onsets_nonconcat.mat'];
-    save(savename2,'names','onsets','onsets_TRs','durations','runs_onsidx');
 end
 %end
 
